585212d816f1fe1be9d064347115e5ab4d0fdf85
Luiggy Macias <ferrinluiggy@gmail.com>
2021-11-04 20:06:58 +0000

    jflñshdkjfhdahlfdsjhf

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,31 +1,10 @@
-import projects from '../data/projects'
-import techs from '../data/technologies'
+import projectsInitial from '../data/projects'
+import techs, {tech_icons} from '../data/technologies'
 import './main.scss'
 import Project from '../Project'
-console.log(projects)
-const Main = () => {
-	return (
-		<div className="main_container">
-			<div className="technologies">
-				{/*<Technologies /> */}
-			</div>
-			<div className='projects'>
-				{projects.map((project: Projects) => <Project {...project} />)}
-			</div>
-		</div>
-	);
-};
-
-const Technologies = () => {
-	return (
-		<div>
-			{ /*techs.map((tech, i) => {
-				return <span title={tech.technologies[i]} className="iconify" data-icon={tech.icon_name[i]} data-inline="false"></span>
-			}) */}
-		</div>
-
-	)
-}
+import { FunctionComponent, useState, useEffect } from 'react'
+//import technologies from '../data/technologies'
+//console.log(projects)
 interface Projects {
 	title: string;
 	img: string;
@@ -35,5 +14,44 @@ interface Projects {
 	project_live: string;
 	technologies: string[];
 }
+interface UpdateCurrentTech {target: { value: string}}
+const Main:FunctionComponent = () => {
+	const [projects, setProjects] = useState(projectsInitial)
+	const [currentTech, setCurrentTech] = useState('null')
+	const updateCurrentTech = (e: any): void => {
+		console.log(e.target.value)
+		//return ''
+	}
+	console.log(currentTech)
+	return (
+		<div className="main_container">
+			<div className="technologies">
+				{/* */}<Technologies onUpdateCurrentTech={updateCurrentTech} />
+			</div>
+			<div className='projects'>
+				{projects.map((project: Projects) => <Project {...project} />)}
+			</div>
+		</div>
+	);
+};
+
+const Technologies = (onUpdateCurrentTech: any) => {
+	return (
+		<div>
+			{
+				techs.map((tech: string, i) => {
+					return (
+						<button type='button' value={tech} onClick={(e) => onUpdateCurrentTech}>
+							<span className="iconify" data-icon={tech_icons[i]} data-inline="false"></span>
+							{tech}
+						</button>
+					)
+				})
+			}
+		</div>
+
+	)
+}
+
 
 export default Main;
--- src/data/technologies.tsx
+++ src/data/technologies.tsx
@@ -1,6 +1,16 @@
-let tech = [
-    'MongoDB',
-    'React'
+type Tech = Array<string>
+let tech: Tech = [
+    'JavaScript', 
+    'Sass', 
+    'React', 
+    'Redux'
 ]
-
+//['logos:javascript', 'logos:sass', "logos:react", "logos:redux"]
+type Tech_Icons = string[] 
+export let tech_icons: Tech_Icons = [
+    'logos:javascript',
+    'logos:sass',
+    "logos:react",
+    "logos:redux"
+]
 export default tech
